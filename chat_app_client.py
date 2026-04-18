3222409e432f0ac4961835c97fa3d3c91d5937d4
Peanut2123650 <p2123650@gmail.com>
2026-04-18 13:33:21 +0530

    create basic server and client

---
+++ chat_app_client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+message = client.recv(1024).decode()
+print(message)
+
+client.close()
---
+++ chat_app_server.py
@@ -0,0 +1,15 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 12345
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print("Server running...")
+
+while True:
+    client, address = server.accept()
+    print(f"Connected with {address}")
+    client.send("Welcome to the server!".encode())
