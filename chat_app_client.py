119fe042d542e58eab26981e7f68072d62b31cc3
Peanut2123650 <p2123650@gmail.com>
2026-04-18 22:23:19 +0530

    feat: add username support

--- chat_app_client.py
+++ chat_app_client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 12345
@@ -6,6 +7,34 @@ PORT = 12345
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-while True:
-    message = input("You: ")
-    client.send(message.encode())
+username = input("Enter your username: ")
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode()
+
+            if message == "USERNAME":
+                client.send(username.encode())
+            else:
+                print(message)
+
+        except:
+            print("Disconnected from server")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        try:
+            message = input("You: ")
+            client.send(message.encode())
+        except:
+            break
+
+
+# Start both threads
+threading.Thread(target=receive).start()
+threading.Thread(target=write).start()
--- chat_app_server.py
+++ chat_app_server.py
@@ -10,22 +10,54 @@ server.listen()
 
 print("Server running...")
 
-def handle_client(client, address):
-    print(f"[NEW CONNECTION] {address}")
+clients = {}   # username : client_socket
+
+
+def broadcast(message, sender_client=None):
+    for client in clients.values():
+        if client != sender_client:
+            try:
+                client.send(message.encode())
+            except:
+                pass
+
+
+def handle_client(client, username):
+    print(f"[NEW CONNECTION] {username}")
 
     while True:
         try:
             message = client.recv(1024).decode()
+
             if not message:
                 break
-            print(f"{address}: {message}")
+
+            print(f"{username}: {message}")
+            broadcast(f"{username}: {message}", client)
+
         except:
             break
 
-    print(f"[DISCONNECTED] {address}")
+    print(f"[DISCONNECTED] {username}")
+
+    del clients[username]
     client.close()
+
+    broadcast(f"{username} left the chat")
+
 
 while True:
     client, address = server.accept()
-    thread = threading.Thread(target=handle_client, args=(client, address))
+
+    # Ask for username
+    client.send("USERNAME".encode())
+    username = client.recv(1024).decode()
+
+    clients[username] = client
+
+    print(f"{username} joined the chat")
+
+    broadcast(f"{username} joined the chat")
+
+    thread = threading.Thread(target=handle_client, args=(client, username))
     thread.start()
